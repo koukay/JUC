3f284464e183c6d460aeb5344de108af915afb90
houkai <hikoukay@163.com>
2020-04-28 20:52:15 +0800

    改造目录结构

---
+++ src/main/java/com/bak/JustTest.java
@@ -0,0 +1,7 @@
+package com.bak;
+
+public class JustTest {
+    public static void main(String[] args) {
+        Object o = new Object();
+    }
+}
---
+++ src/main/java/com/bak/juc/c_012_Volatile/ClassLoader1.java
@@ -0,0 +1,30 @@
+/**
+ * volatile �ؼ��֣�ʹһ�������ڶ���̼߳�ɼ�
+ * A B�̶߳��õ�һ��������javaĬ����A�߳��б���һ��copy���������B�߳��޸��˸ñ�������A�߳�δ��֪��
+ * ʹ��volatile�ؼ��֣����������̶߳�������������޸�ֵ
+ * 
+ * ������Ĵ����У�running�Ǵ����ڶ��ڴ��t������
+ * ���߳�t1��ʼ���е�ʱ�򣬻��runningֵ���ڴ��ж���t1�̵߳Ĺ������������й�����ֱ��ʹ�����copy��������ÿ�ζ�ȥ
+ * ��ȡ���ڴ棬�����������߳��޸�running��ֵ֮��t1�̸߳�֪���������Բ���ֹͣ����
+ * 
+ * ʹ��volatile������ǿ�������̶߳�ȥ���ڴ��ж�ȡrunning��ֵ
+ * 
+ * �����Ķ���ƪ���½��и���������
+ * http://www.cnblogs.com/nexiyi/p/java_memory_model_and_thread.html
+ * 
+ * volatile�����ܱ�֤����̹߳�ͬ�޸�running����ʱ�������Ĳ�һ�����⣬Ҳ����˵volatile�������synchronized
+ * @author mashibing
+ */
+package com.bak.juc.c_012_Volatile;
+
+public class ClassLoader1 {
+
+
+	public static void main(String[] args) {
+		ClassLoader1 t = new ClassLoader1();
+
+		System.out.println(ClassLoader.getSystemClassLoader());
+
+	}
+
+}
